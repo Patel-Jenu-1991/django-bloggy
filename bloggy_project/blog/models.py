eb63124bb0f62bf92347105245c4bf1572717ed2
Jenu Patel <jenu.16@icloud.com>
2017-06-27 17:26:37 -0500

    Setup the model - defines the db table and its fields

--- bloggy_project/blog/models.py
+++ bloggy_project/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# class Post inherits from the standard Django Model() class
+# It defines the database as well as each field:
+# created_at, title, content - representing a single blog post
+
+class Post(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
