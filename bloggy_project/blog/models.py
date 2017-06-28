1b9d476d33796e40d41414cbf9755478f41e951f
Jenu Patel <jenu.16@icloud.com>
2017-06-27 22:54:54 -0500

    Added a method to display object info

--- bloggy_project/blog/models.py
+++ bloggy_project/blog/models.py
@@ -10,3 +10,11 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
+
+    # method to return distinguishing information
+    # about the object
+    def __str__(self):
+        # returns the complete object details
+        # return "{0}/{1}/{2}/{3}\n".format(self.id, self.created_at,\
+        #   self.title, self.content)
+        return self.title   # returns only the Post title
