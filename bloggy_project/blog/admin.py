7e44782dc235e3c606a8d9b80064673a0a53dcb5
Jenu Patel <jenu.16@icloud.com>
2017-06-27 23:21:59 -0500

    Registered classes for custom admin view

--- bloggy_project/blog/admin.py
+++ bloggy_project/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from blog.models import Post
+
+# Customize admin views here
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
 
 # Register your models here.
+
+admin.site.register(Post, PostAdmin)
