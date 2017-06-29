89fc5c29d19b9e23125e89650d6eb6767d219f01
Jenu Patel <jenu.16@icloud.com>
2017-06-29 15:28:53 -0500

    Updated the views to clean up stinky code

--- bloggy_project/blog/views.py
+++ bloggy_project/blog/views.py
@@ -5,48 +5,48 @@ from django.shortcuts import get_object_or_404, render, redirect
 from blog.models import Post
 from blog.forms import PostForm
 
-# Create your views here.
 
-# helper function
-def encode_url(url):
-    return url.replace(' ', '_')
+####################
+# helper functions #
+####################
+
+def get_popular_posts():
+    popular_posts = Post.objects.order_by('-views')[:5]
+    return popular_posts
+
+
+##################
+# view functions #
+##################
 
 def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
-    # gets the top 5 popular posts from blog_posts table
-    # sorted by views
-    popular_posts = Post.objects.order_by('-views')[:5]
     t = loader.get_template('blog/index.html')
     context_dict = {
         'latest_posts': latest_posts,
-        'popular_posts': popular_posts,
+        'popular_posts': get_popular_posts(),
     }
-    for post in latest_posts:
-        post.url = encode_url(post.title)
-    for popular_post in popular_posts:
-        popular_post.url = encode_url(popular_post.title)
     c = Context(context_dict)
     return HttpResponse(t.render(c))
 
-def post(request, post_url):
-    popular_posts = Post.objects.order_by('-views')[:5]
-    single_post = get_object_or_404(Post, title=post_url.replace('_', ' '))
-    single_post.views += 1      # increment the number of views
-    single_post.save()          # and save it
+
+def post(request, slug):
+    single_post = get_object_or_404(Post, slug=slug)
+    single_post.views += 1  # increment the number of views
+    single_post.save()      # and save it
+    t = loader.get_template('blog/post.html')
     context_dict = {
         'single_post': single_post,
-        'popular_posts': popular_posts,
+        'popular_posts': get_popular_posts(),
     }
-    t = loader.get_template('blog/post.html')
     c = Context(context_dict)
     return HttpResponse(t.render(c))
 
-# Logic for handling of the form
 
 def add_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
-        if form.is_valid():     # is the form valid?
+        if form.is_valid():  # is the form valid?
             form.save(commit=True)  # yes? save to database
             return redirect(index)
         else:
