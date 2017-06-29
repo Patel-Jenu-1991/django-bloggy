7789fab7a35fbe9ad3d04157d1f96146d80f972a
Jenu Patel <jenu.16@icloud.com>
2017-06-29 15:13:16 -0500

    Created and applied migrations for using a unique slug for the URL

--- bloggy_project/blog/models.py
+++ bloggy_project/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from uuslug import uuslug
+
 # Create your models here.
 
 # class Post inherits from the standard Django Model() class
@@ -13,6 +15,7 @@ class Post(models.Model):
     tag = models.CharField(max_length=20, blank=True, null=True)
     image = models.ImageField(upload_to="images", blank=True, null=True)
     views = models.IntegerField(default=0)
+    slug = models.CharField(max_length=100, unique=True)
 
     # method to return distinguishing information
     # about the object
@@ -21,3 +24,7 @@ class Post(models.Model):
         # return "{0}/{1}/{2}/{3}\n".format(self.id, self.created_at,\
         #   self.title, self.content)
         return self.title   # returns only the Post title
+
+    def save(self, *args, **kwargs):
+        self.slug = uuslug(self.title, instance=self, max_length=100)
+        super(Post, self).save(*args, **kwargs)
