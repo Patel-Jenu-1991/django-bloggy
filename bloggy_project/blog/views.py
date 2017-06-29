9dfbb8f3e44e2373b4a8f875eee57a58145194c9
Jenu Patel <jenu.16@icloud.com>
2017-06-29 01:01:25 -0500

    Added a helper function to encode urls

--- bloggy_project/blog/views.py
+++ bloggy_project/blog/views.py
@@ -6,8 +6,14 @@ from blog.models import Post
 
 # Create your views here.
 
+# helper function
+def encode_url(url):
+    return url.replace(' ', '_')
+
 def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
+    # gets the top 5 popular posts from blog_posts table
+    # sorted by views
     popular_posts = Post.objects.order_by('-views')[:5]
     t = loader.get_template('blog/index.html')
     context_dict = {
@@ -15,9 +21,9 @@ def index(request):
         'popular_posts': popular_posts,
     }
     for post in latest_posts:
-        post.url = post.title.replace(' ', '_')
+        post.url = encode_url(post.title)
     for popular_post in popular_posts:
-        popular_post.url = popular_post.title.replace(' ', '_')
+        popular_post.url = encode_url(popular_post.title)
     c = Context(context_dict)
     return HttpResponse(t.render(c))
 
